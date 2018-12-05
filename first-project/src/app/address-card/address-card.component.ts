7e39b4edffa8d62517445f2abf56e2c9a899b4e0
gurvsxen487 <gauravsaxena.cs@gmail.com>
2018-12-05 23:12:43 +0530

    click handling

--- first-project/src/app/address-card/address-card.component.ts
+++ first-project/src/app/address-card/address-card.component.ts
@@ -7,13 +7,13 @@ import { User } from './user.model';
   styleUrls: ['./address-card.component.css']
 })
 export class AddressCardComponent implements OnInit {
-// 
-  // user: any;
-  //pass string as a parameter
-  // @Input('user') userobj: User;
   
-  //get an object fromapp-root component
   @Input('user') user: User;
+  isCollapsed:boolean = true;
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 
 
   constructor() {
