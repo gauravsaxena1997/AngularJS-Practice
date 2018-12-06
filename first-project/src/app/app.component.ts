313120f343e43c6b2de14dcff075bedc67ef8e24
gurvsxen487 <gauravsaxena.cs@gmail.com>
2018-12-06 13:09:23 +0530

    making REST call with HttpClient

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 import { User } from './address-card/user.model';
 import { TestService } from './test.service';
 
@@ -12,20 +15,35 @@ export class AppComponent {
 	textInput:string = "Initial Value";
   service:string = "";
 
-	constructor(private svc: TestService) {
- 
-   this.service = this.svc.printToConsole('Got the service !!');
+	constructor(private svc: TestService, private http: HttpClient) {
+    this.service = this.svc.printToConsole('Got the service !!');
 
-
-	this.user = new User();
+    this.user = new User();
       this.user.name= 'Gaurav Saxena',
       this.user.desigination= 'Front-End Developer',
       this.user.address= '144-A, Baker Street, New York',
       this.user.contact= [
         '+91-9549992949',
         '+91-7976761580' ];
-
-	}
+  }
+  ngOnInit() {
+    let obs =  this.http.get('https://api.github.com/users/gurvsxen487');
+    obs.subscribe((response) => console.log(response));
+  } 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
